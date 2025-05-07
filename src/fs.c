d2eabc31fd9d46193b17cff606bc88a65663791b
SamPatatoz <issamissam@hotmail.be>
2025-05-07 14:20:46 +0200

    Added Makefile

---
+++ src/fs.c
@@ -0,0 +1,34 @@
+#include <stdint.h>
+
+int format(char *disk_name, int inodes)
+{
+	
+}
+int stat(int inode_num)
+{
+
+}
+int mount(char *disk_name)
+{
+
+}
+int unmount()
+{
+
+}
+int create()
+{
+
+}
+int delete(int inode_num)
+{
+
+}
+int read(int inode_num, uint8_t *data, int len, int offset)
+{
+
+}
+int write(int inode_num, uint8_t *data, int len, int offset)
+{
+
+}
